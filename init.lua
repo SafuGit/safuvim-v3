9cd783b92b350f54e382e906fae1a694e6eb3f2e
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2026-01-12 17:05:58 +0600

    add lazy.nvim

---
+++ init.lua
@@ -0,0 +1,14 @@
+require("config.lazy")
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 8
+vim.opt.clipboard = "unnamedplus"
+
+-- Leader key
+vim.g.mapleader = " "
