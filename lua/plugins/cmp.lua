8873c4fd18a2f1560e5dfafab403dcd460c66626
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2026-01-13 16:56:56 +0600

    feat: update nvim-cmp dependencies and fix typo in event name

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,7 +3,7 @@ return {
   dependencies = {
     "hrsh7th/cmp-buffer", -- buffer completion,
     "hrsh7th/cmp-path", -- filesystem paths,
-    { "hrsh7th/cmp-nvim-lsp", event = { "BufReadPre", "BufNewFilez" } },  -- lsp completions
+    { "hrsh7th/cmp-nvim-lsp", event = { "BufReadPre", "BufNewFile" } },  -- lsp completions
     "hrsh7th/cmp-nvim-lsp-signature-help",
     "hrsh7th/cmp-nvim-lua", -- neovim lua api completions
     "petertriho/cmp-git", -- git completions
