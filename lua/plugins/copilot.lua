1d75a7737f3b90694f3531b381e44893f1c863d7
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2026-01-13 17:29:22 +0600

    feat: update Copilot plugin configuration and dependencies

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,13 +1,20 @@
 return {
   {
-    "github/copilot.vim",
+    "zbirenbaum/copilot.lua",
+    dependencies = {
+      "copilotlsp-nvim/copilot-lsp", -- (optional) for NES functionality
+    },
+    cmd = "Copilot",
     event = "InsertEnter",
+    config = function()
+      require("copilot").setup({})
+    end,
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     dependencies = {
       { "nvim-lua/plenary.nvim", branch = "master" },
-      { "gptlang/lua-tiktoken", branch = "main" }
+      { "gptlang/lua-tiktoken",  branch = "main" }
     },
     build = "make tiktoken",
     opts = {
