e2314da4e06c854c25b190faf8c017a49a5e8432
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2026-01-13 16:56:24 +0600

    feat: add nvim-cmp plugin with configuration and dependencies for enhanced completion

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,33 @@
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-buffer", -- buffer completion,
+    "hrsh7th/cmp-path", -- filesystem paths,
+    { "hrsh7th/cmp-nvim-lsp", event = { "BufReadPre", "BufNewFilez" } },  -- lsp completions
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+    "hrsh7th/cmp-nvim-lua", -- neovim lua api completions
+    "petertriho/cmp-git", -- git completions
+    "SergioRibera/cmp-dotenv", -- .env file completions
+  },
+  config = function()
+    local cmp = require("cmp")
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local capabilities = cmp_nvim_lsp.default_capabilities()
+
+    vim.lsp.config("*", {
+      capabilities = capabilities,
+    })
+
+    cmp.setup({
+      sources = cmp.config.sources({
+        { name = "buffer" },
+        { name = "path" },
+        { name = "nvim_lsp" },
+        { name = "nvim_lsp_signature_help" },
+        { name = "nvim_lua" },
+        { name = "git" },
+        { name = "dotenv" },
+      })
+    })
+  end
+}
