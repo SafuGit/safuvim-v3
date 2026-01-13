20adfb23852d640db97abaaa85ea240954e49d60
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2026-01-13 15:29:37 +0600

    remove telescope

--- lua/plugins/finder.lua
+++ lua/plugins/finder.lua
@@ -1,18 +1,33 @@
 return {
+  -- {
+  --   'nvim-telescope/telescope.nvim',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     -- optional but recommended
+  --     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  --   }
+  -- },
+  -- {
+  --   'axkirillov/easypick.nvim',
+  --   dependencies = { 'nvim-telescope/telescope.nvim' },
+  -- },
+  -- {
+  --   'nvim-telescope/telescope-media-files.nvim',
+  --   dependencies = { 'nvim-telescope/telescope.nvim' },
+  -- }
+
   {
-    'nvim-telescope/telescope.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      -- optional but recommended
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    "folke/snacks.nvim",
+    ---@diagnostic disable-next-line: undefined-doc-name
+    ---@type snacks.Config
+    opts = {
+      picker = {
+        -- your picker configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+
+        -- *TODO - Setup Finder keybinds with built-in pickers with my custom command pallete
+      }
     }
-  },
-  {
-    'axkirillov/easypick.nvim',
-    dependencies = { 'nvim-telescope/telescope.nvim' },
-  },
-  {
-    'nvim-telescope/telescope-media-files.nvim',
-    dependencies = { 'nvim-telescope/telescope.nvim' },
   }
 }
