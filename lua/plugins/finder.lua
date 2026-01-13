8873ac76153e71ad70886b9c0142d881a291dc20
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2026-01-13 17:38:29 +0600

    add find and replace

--- lua/plugins/finder.lua
+++ lua/plugins/finder.lua
@@ -34,5 +34,14 @@ return {
   {
     'tamago324/lir.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
+  },
+
+  {
+    "mahyarmirrashed/search-and-replace.nvim",
+    config = function()
+      require("search-and-replace").setup()
+    end,
   }
+
+  -- *TODO - Configure search and replace in command pallete
 }
