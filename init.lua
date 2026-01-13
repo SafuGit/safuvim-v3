7f03f00d9bf6e85e7684ce4a11d848bb8dd30179
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2026-01-13 17:23:56 +0600

    feat: add key mappings for clipboard operations and select all functionality

--- init.lua
+++ init.lua
@@ -9,7 +9,15 @@ vim.opt.tabstop = 2
 vim.opt.termguicolors = true
 vim.opt.cursorline = true
 vim.opt.scrolloff = 8
+
 vim.opt.clipboard = "unnamedplus"
+vim.keymap.set("i", "<C-v>", "<C-r>+", { noremap = true })
+vim.keymap.set("n", "<C-v>", '"+p', { noremap = true })
+vim.keymap.set("v", "<C-v>", '"+p', { noremap = true })
+vim.keymap.set("v", "<C-c>", '"+y', { noremap = true })
+
+vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true })
+vim.keymap.set("i", "<C-a>", "<Esc>ggVG", { noremap = true })
 
 -- Leader key
 vim.g.mapleader = " "
