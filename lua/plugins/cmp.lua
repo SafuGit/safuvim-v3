50b8a77cf5c6a4b9cbf71874d94112f6552871df
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2026-01-14 17:04:58 +0600

    feat(cmp): enhance key mappings for navigation and selection in completion menu

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -19,6 +19,31 @@ return {
     })
 
     cmp.setup({
+      mapping = cmp.mapping.preset.insert({
+        ["<Down>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        ["<Up>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.confirm({ select = true }) -- inserts the selected item
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
       sources = cmp.config.sources({
         { name = "buffer" },
         { name = "path" },
