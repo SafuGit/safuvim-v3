ff736ef5f6f8f289ae1f25359cd87f2b9e212298
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2026-01-14 12:18:37 +0600

    feat: add fzf-lua and project.nvim plugin configurations; enhance command palette with project view

--- lua/plugins/command_pallete/cpl.lua
+++ lua/plugins/command_pallete/cpl.lua
@@ -1,22 +1,31 @@
 return {
-  'AtleSkaanes/command-palette.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  version = "*",
-  opts = {
-    commands = {
-      {
-        name = "Open File Explorer (LIR)",
-        category = "Files",
-        cmd = function()
-          require("lir.float").toggle()
-        end,
-      }
-    },
+  {
+    'AtleSkaanes/command-palette.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    version = "*",
+    opts = {
+      commands = {
+        {
+          name = "Open File Explorer (LIR)",
+          category = "Files",
+          cmd = function()
+            require("lir.float").toggle()
+          end,
+        },
+        {
+          name = "View Projects Palette",
+          category = "Files",
+          cmd = function ()
+            vim.cmd("Project")
+          end
+        }
+      },
 
-    icons = {
-      category = "󰉋",
-      cmd = "󰘳 ",
-      back = "← Go Back",
+      icons = {
+        category = "󰉋",
+        cmd = "󰘳 ",
+        back = "← Go Back",
+      }
     }
-  }
+  },
 }
